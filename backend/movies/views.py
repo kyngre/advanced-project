297d14872704b2df8f87504e71e540ca596390d6
Taeseong Choi <slimshady913@naver.com>
2025-05-04 16:47:57 +0900

    ordering

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Movie
 from .serializers import MovieSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -7,6 +7,9 @@ class MovieListCreateView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.OrderingFilter]  # ordering 기능 활성화
+    ordering_fields = ['average_rating', 'release_date', 'title']  # 지원 항목
+    ordering = ['-average_rating']  # 기본 정렬 (선택)
 
     def perform_create(self, serializer):
         movie = serializer.save()
